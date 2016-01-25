89570516dfcbae70fccd5faff45fa33ce5fde849
danieltl <vague0213@gmail.com>
2016-01-25 13:29:01 +0900

    First Commit Building own Lib.

---
+++ 01.LCM(Least Common Multiple)/lcm.h
@@ -0,0 +1,16 @@
+//
+// Created by daniel on 16. 1. 25.
+//
+
+
+
+int pNumcmp(int *num1, int *num2) {
+    return (*num1 > *num2) ? *num1 : *num2;
+}
+
+
+int Lcm() {
+    int num1 = 0, num = 0, result = 0;
+
+
+}
